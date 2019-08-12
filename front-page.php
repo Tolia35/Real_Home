875207c9bc76bbb24a1d63066b38df232b48a5b4
Tolia <anatolia060590@hotmail.com>
2019-08-12 15:46:30 +0200

    Ajout et mep logo reseaux soc + début boucle

--- front-page.php
+++ front-page.php
@@ -2,64 +2,69 @@
 /**
  * The main template file
  *
- * ...
  *
  * @package scratch
- *
  */
-
-$lastposts = get_posts(array(
+$lastproperty = get_posts( array(
 	'numberposts' => 6,
-	'post_type' => 'property',
-));
+  'post_type' => 'propritété',
+  'orderby' => 'rand'
+) );
 
-get_header();
+get_header(); 
 ?>
 
-<main class="py-6>"
+<main>
 
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+  <article <?php post_class(); ?>>
+      <?php the_content() ?>
+  </article>
+  <?php endwhile; ?>
 
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+  <?php else : ?>
+  <div class="container py-5">
+    <p><?php _e( 'Sorry, no posts matched your criteria.', 'scratch' ); ?></p>
+  </div>
+  <?php endif; ?>
 
-		<article class="container">
-			<h1 class="entry-title">
-				<?php the_title(); ?>
-			</h1>
-
-			<?php if (has_post_thumbnail()) : ?>
-				<div class="row flex-md-row-reverse">
-					<div class="col-md-6 col-lg-4">
-						<?php the_post_thumbnail('thumb-730', array('class'=>'img-fluid')); ?>
-					</div>
-					<div class="col-md-6 col-lg-8">
-						<?php the_content() ?>
-					</div>
-				</div>
-			<?php else : ?>
-				<?php the_content() ?>
-			<?php endif; ?>
-		</article>
-
-
-		<?php foreach ($lastposts as $post) : ?>
-			<?php setup_postdata($post); ?>
-
-            <article class="container" <?php post_class(); ?>>
-                <figure>
-                    <a href="<?php the_permalink() ?>"><?php the_post_thumbnail('medium') ?></a>
-                </figure>
-                <h3><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
-                <p><?php the_excerpt() ?></p>
-            </article>
-
-		<?php endforeach; ?>
-		<?php wp_reset_postdata(); ?>
-
-	<?php endwhile; ?>
-	<?php else : ?>
-		<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-	<?php endif; ?>
-	
 </main>
-<?php get_footer() ?>
 
+<section class="container">
+  <?php if ( $lastproperty ) : ?>
+    <div class="front-proprietes_grid">
+      <?php foreach ( $lastproperty as $post ) :
+          setup_postdata( $post );	
+
+          get_template_part( 'template-parts/content', 'property' );
+
+      endforeach; 
+      wp_reset_postdata(); ?>
+    </div>
+  <?php endif;?>
+  <div class="text-center">
+    <a href="<?= esc_url( home_url( '/' ) ) ?>/propriete/" class="btn btn-outline-primary my-5"><?php _e('Toutes les propriétés', 'scratch'); ?></a>
+  </div>
+</section>
+
+  <?php if ( $lastposts ) : ?>
+
+    <section class="front-sticky-post container my-5">
+      <?php foreach ( $lastposts as $post ) :
+          setup_postdata( $post );	?>
+
+        <article <?php post_class('sticky-post_article row'); ?>>
+          <div class="sticky-post_content py-4 bg-white col-sm-10 offset-sm-1 col-md-5 offset-md-0 ">
+            <h2><?php _e('À la une', 'scratch'); ?></h2>
+            <h3 class="h4"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+            <?php the_excerpt(); ?>
+          </div>
+        </article>
+
+      <?php endforeach; 
+      wp_reset_postdata(); ?>
+  </section>
+
+<?php endif;?>
+
+<?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,17 +1,42 @@
 <?php
 /**
- * The main template file
+ * The template for displaying all single posts
  *
- * ...
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
- * @package scratch
- *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
  */
+
+$champ_prix = get_field_object('prix');
+$champ_ville = get_field_object('ville');
+$champ_surface = get_field_object('surface');
+$champ_infos = get_field_object('infos');
+$champ_illustration = get_field_object('illustration');
+$champ_description = get_field_object('description');
+
+
 get_header();
 ?>
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <?php endwhile; ?>
-    <?php else : ?>
-        <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-    <?php endif; ?>
-<?php get_footer() ?>
+
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+<article <?php post_class('card-propriete-article'); ?>>
+    <div class="card-propriete_content p-3">
+	  <?php the_title( '<h2 class="entry-title h4">', '</h2>' ); ?>
+	 <strong><?= $champ_prix['value'] ?> <?= $champ_prix['append'] ?></strong></p>
+	  <?= $champ_ville['label'] ?> : <strong><?= $champ_ville['value'] ?></strong></p>
+	  <?= $champ_surface['label'] ?> : <strong><?= $champ_surface['value'] ?></strong></p>
+	  <?= $champ_infos['label'] ?> : <strong><?= $champ_infos['value'] ?></strong></p>
+	  <?= $champ_illustration['label'] ?> : <strong><?= $champ_illustration['value'] ?></strong></p>
+	  <strong><?= $champ_description['value'] ?></strong>
+    </div>
+</article>
+
+		</main><!-- #main -->
+	</section><!-- #primary -->
+
+<?php
+get_footer();
